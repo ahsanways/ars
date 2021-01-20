b162a9da9629f2c8788fa2d937d051d67d8ca9fa
SANJEEVAN\Sanjeevan <sanjeevan_sigdel@yahoo.com>
2021-01-19 22:18:15 -0600

    Populated Address class with Constructors, getters and setters

--- src/frs/Address.java
+++ src/frs/Address.java
@@ -1,5 +1,61 @@
 package frs;
 
 public class Address {
+	int id;
+	String street;
+	String city;
+	String state;
+	String zipcode;
+
+	public Address(int id, String street, String city, String state, String zipcode) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zipcode = zipcode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
+	
 
 }
