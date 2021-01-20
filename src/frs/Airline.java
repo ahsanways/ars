182f22cc5f724e454820d4193c2438259247a063
ahsanways <ahsanhere007@gmail.com>
2021-01-20 01:21:06 -0600

    Added getters setters, overridden toString Functions and created constructors.

--- src/frs/Airline.java
+++ src/frs/Airline.java
@@ -1,5 +1,48 @@
 package frs;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Airline {
+	String id;
+	String code;
+	String name;
+	String history;
+	List<Flight> flights;
+	
+	public Airline(String id, String code, String name, String history) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.name = name;
+		this.history = history;
+		this.flights = new ArrayList<Flight>();
+	}
 
+	public String getId() {
+		return id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getHistory() {
+		return history;
+	}
+
+	public List<Flight> getFlights() {
+		return flights;
+	}
+
+	@Override
+	public String toString() {
+		return "Airline [id=" + id + ", code=" + code + ", name=" + name + ", history=" + history + ", flights="
+				+ flights + "]";
+	}
+	
+	
 }
--- src/frs/FlightInstance.java
+++ src/frs/FlightInstance.java
@@ -1,5 +1,62 @@
 package frs;
-
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 public class FlightInstance {
+	String id;
+	LocalDate date;
+	Flight flight;
+	List<Reservation> reservations;
+	
+	
+	public FlightInstance(String id, LocalDate date, Flight flight) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.flight = flight;
+		this.reservations = new ArrayList<Reservation>();
+	}
 
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+
+	public Flight getFlight() {
+		return flight;
+	}
+
+
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FlightInstance [id=" + id + ", date=" + date + ", flight=" + flight + ", reservations=" + reservations
+				+ "]";
+	}
+	
 }
